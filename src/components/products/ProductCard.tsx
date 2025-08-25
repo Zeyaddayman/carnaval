6d79049942148414615a4e996876e3ebe31a8be7
Zeyad <zezoayman0100@yahoo.com>
2025-08-25 18:34:12 +0300

    Add: AddToWishlistButton component and add it to the prodcut card with no functionality

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -2,6 +2,7 @@ import { CardProduct } from "@/types/products"
 import Image from "next/image"
 import Link from "next/link"
 import RatingStars from "../ui/RatingStars"
+import AddToWishlistButton from "../product/AddToWishlistButton"
 
 const ProductCard = ({ product }: { product: CardProduct }) => {
 
@@ -12,38 +13,42 @@ const ProductCard = ({ product }: { product: CardProduct }) => {
         : Number(product.price)
 
     return (
-        <Link
-            key={product.id}
-            href={`/product/${product.id}`}
-            className="p-3 w-78 flex flex-col border border-border rounded-lg"
-        >
-            <div
-                className="relative w-full h-78 aspect-square"
+        <div key={product.id} className="relative">
+            <span className="absolute top-3 right-3 z-20">
+                <AddToWishlistButton />
+            </span>
+            <Link
+                href={`/product/${product.id}`}
+                className="p-3 w-78 flex flex-col bg-card border border-border rounded-lg"
             >
-                <Image
-                    src={product.thumbnail}
-                    alt={product.title}
-                    fill
-                    sizes="312px"
-                    className="rounded-md border border-border"
-                />
-            </div>
-            <h3 className="text-lg text-foreground font-semibold mt-2 mb-1">{product.title}</h3>
-            {product.brand && <p className="text-muted-foreground">by <span>{product.brand.name}</span></p>}
-            <div className="flex-1 flex items-end flex-wrap gap-3 mt-2 mb-4">
-                <p className="text-lg font-semibold text-foreground">${finalPrice.toFixed(2)}</p>
-                {hasDiscount && (
-                    <>
-                    <p className="text-lg text-muted-foreground line-through">${Number(product.price).toFixed(2)}</p>
-                    <p className="bg-success/10 text-success text-sm p-1 rounded-lg">-{product.discountPercentage}%</p>
-                    </>
-                )}
-            </div>
-            <div className="flex gap-2 items-center">
-                <RatingStars rating={product.rating} />
-                <span className="text-muted-foreground">{product.rating}</span>
-            </div>
-        </Link>
+                <div
+                    className="relative w-full h-78 aspect-square"
+                >
+                    <Image
+                        src={product.thumbnail}
+                        alt={product.title}
+                        fill
+                        sizes="312px"
+                        className="rounded-md border border-border"
+                    />
+                </div>
+                <h3 className="text-lg text-card-foreground font-semibold mt-2 mb-1">{product.title}</h3>
+                {product.brand && <p className="text-muted-foreground">by <span>{product.brand.name}</span></p>}
+                <div className="flex-1 flex items-end flex-wrap gap-3 mt-2 mb-4">
+                    <p className="text-lg font-semibold text-card-foreground">${finalPrice.toFixed(2)}</p>
+                    {hasDiscount && (
+                        <>
+                        <p className="text-lg text-muted-foreground line-through">${Number(product.price).toFixed(2)}</p>
+                        <p className="bg-success/10 text-success text-sm p-1 rounded-lg">-{product.discountPercentage}%</p>
+                        </>
+                    )}
+                </div>
+                <div className="flex gap-2 items-center">
+                    <RatingStars rating={product.rating} />
+                    <span className="text-muted-foreground">{product.rating}</span>
+                </div>
+            </Link>
+        </div>
     )
 }
 
