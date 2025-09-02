d88ddc7acf55341a2b471afb9e556d711847ce6b
Zeyad <zezoayman0100@yahoo.com>
2025-09-02 17:49:36 +0300

    Add: Input component and add backdrop to the Dialog component

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { Dialog as HeadlessDialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog as HeadlessDialog, DialogPanel, DialogTitle, DialogBackdrop } from '@headlessui/react'
 import { ReactNode } from 'react'
 import { Button } from './Button'
+import { FiX } from 'react-icons/fi';
 
 interface Props {
     isOpen: boolean;
@@ -14,15 +15,21 @@ interface Props {
 export default function Dialog({ isOpen, close, title, children }: Props) {
     return (
         <HeadlessDialog open={isOpen} as="div" className="relative z-50 focus:outline-none" onClose={close} __demoMode>
+            <DialogBackdrop className="fixed inset-0 bg-black/50" />
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-center justify-center p-4">
                     <DialogPanel
                         transition
-                        className="w-full max-w-md rounded-xl bg-muted border-border shadow-xl p-6 duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+                        className="w-full max-w-md rounded-xl bg-white border-border shadow-xl p-6 duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
                     >
-                        <DialogTitle as="h3" className="text-base/7 font-medium text-foreground">
-                            { title }
-                        </DialogTitle>
+                        <div className='flex justify-between items-center mb-5'>
+                            <DialogTitle as="h3" className="text-xl font-semibold text-foreground">
+                                { title }
+                            </DialogTitle>
+                            <Button variant="ghost" onClick={close}>
+                                <FiX size={20} />
+                            </Button>
+                        </div>
 
                         { children }
 
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils"
+
+function Input({ className, ...props }: React.ComponentProps<"input">) {
+    return (
+        <input
+            data-slot="input"
+            className={cn(
+                "bg-input border-border flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base md:text-sm shadow-xs transition-[color,box-shadow] placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+                "focus-visible:ring-primary focus-visible:ring-2",
+                "aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+export default Input
