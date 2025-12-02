ee1cbe8c3ae0f32594057ac25a8fda4e7a3ea076
Zeyad <zezoayman0100@yahoo.com>
2025-12-02 04:34:56 +0200

    Feature: show user his orders summary section in account overview profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 import { buttonVariants } from "@/components/ui/Button"
 import Heading from "@/components/ui/Heading"
+import { getUserOrdersSummary } from "@/server/db/orders"
 import { getProfile, getUserDefaultAddress } from "@/server/db/profile"
 import Link from "next/link"
 
@@ -11,6 +12,7 @@ const ProfileAccountOverviewPage = () => {
         />
         <div className="grid md:grid-cols-2 gap-5">
             <PersonalInformationSection />
+            <OrdersSummary />
             <ShippingAddressSection />
         </div>
         </>
@@ -35,6 +37,30 @@ const PersonalInformationSection = async () => {
             >
                 Edit Profile
             </Link>
+        </section>
+    )
+}
+
+const OrdersSummary = async () => {
+
+    const { totalOrders, pendingOrders, lastOrderDate } = await getUserOrdersSummary()
+
+    if (totalOrders === 0) return
+
+    return (
+        <section className="px-3 py-6 flex flex-col bg-card border border-border rounded-md">
+            <h5 className="text-xl font-semibold mb-5">Orders Summary</h5>
+                <div className="flex flex-col gap-3 text-muted-foreground mb-6">
+                    <p>Total Orders: {totalOrders}</p>
+                    <p>Last Order Date: {new Date(lastOrderDate).toLocaleDateString()}</p>
+                    <p>Pending Orders: {pendingOrders}</p>
+                </div>
+                <Link
+                    href={"/profile/orders"}
+                    className={`${buttonVariants({ variant: "secondary" })} mt-auto`}
+                >
+                    View Orders
+                </Link>
         </section>
     )
 }
